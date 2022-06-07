fe5717b541b2d8413ca1f776dd82184b58038bf6
Azimi2kht <s.h.azimidokht80@gmail.com>
2022-06-07 21:56:08 +0430

    Complete recognition of Indirect instructions

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ instructions = {
     'BSA': '0101',
     'ISZ': '0110',
 
-    'AND I': '1000',
-    'ADD I': '1001',
-    'LDA I': '1010',
-    'STA I': '1011',
-    'BUN I': '1100',
-    'BSA I': '1101',
-    'ISZ I': '1110',
+    'ANDI': '1000',
+    'ADDI': '1001',
+    'LDAI': '1010',
+    'STAI': '1011',
+    'BUNI': '1100',
+    'BSAI': '1101',
+    'ISZI': '1110',
 
     # register reference
     'CLA': '0111',
@@ -47,7 +47,7 @@ content = file.read()
 lines = content.splitlines()
 
 lineCounter = 0
-flag = True
+scale = 16
 
 # first time reading:
 index = 0
@@ -55,16 +55,24 @@ while index < len(lines):
     lines[index] = onlyOneSpace = ' '.join(lines[index].split())
     if 'ORG' in lines[index]:
         lineCounter = int(onlyOneSpace[3:])
-        # delete the line
+        # remove the line
         lines.pop(index)
         index -= 1
-    elif ',' in lines[index]:
+    elif ' I' in lines[index]:
+        if ',' in lines[index]:
+            variableTable[lineCounter] = onlyOneSpace[0:onlyOneSpace.find(',')]
+        instruction = lines[index][lines[index].find(',') + 1: lines[index].find(' ')]
+        lines[index] = lines[index].replace(instruction, instructions[instruction+'I'])
+        lines[index] = lines[index][lines[index].find(',') + 1:]
+        lines[index] = lines[index].replace('I', '')
+
+    elif ',' in lines[index] or 'HEX' in lines[index] or 'DEC' in lines[index]:
         variableTable[lineCounter] = onlyOneSpace[0:onlyOneSpace.find(',')]
-        # delete the line
+        # remove the line
         lines.pop(index)
         index -= 1
     elif 'END' in lines[index]:
-        # delete the line
+        # remove the line
         lines.pop(index)
         index -= 1
         break
@@ -72,13 +80,20 @@ while index < len(lines):
         instruction = lines[index][0:3]
         if instruction in instructions:
             lines[index] = lines[index].replace(instruction, instructions[instruction])
+
     index += 1
     lineCounter += 1
 
 # second time reading:
 for index in range(len(lines)):
     if len(lines[index].split()) > 1:
-        lines[index] = lines[index].replace(lines[index].split()[1], str(list(variableTable.keys())[list(variableTable.values()).index(lines[index].split()[1])]))
+        try:
+            hexAddress = list(variableTable.keys())[list(variableTable.values()).index(lines[index].split()[1])]
+            binaryAddress = bin(int(str(hexAddress), scale))[2:].zfill(12)
+            lines[index] = lines[index].replace(lines[index].split()[1], str(
+                binaryAddress))
+        except ValueError:
+            pass
 
 # printing the result
 for line in lines:
