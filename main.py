7b0b7db4ae5a2d3c366a163d2d636feb2faf836e
Azimi2kht <s.h.azimidokht80@gmail.com>
2022-06-08 21:34:49 +0430

    Fix the negative numbers problem

--- main.py
+++ main.py
@@ -1,39 +1,31 @@
 import re
+
+
 # TODO: handle the cases where a non-memory reference instruction is used with indirect
-def flip(c):
-    return '1' if (c == '0') else '0'
+def decimal_to_binary(num: int, width: int):
+    if num >= 0:
+        return bin(num)[2:].zfill(width)
+    else:
+        # convert to binary first
+        pos_binary = bin(abs(num))[2:].zfill(width)
 
+        num_of_bits = len(pos_binary)
+        temp_list = list(pos_binary)
+        index = num_of_bits - 1
+        flag = False
+        while index >= 0:
+            if temp_list[index] == '1' and not flag:
+                flag = True
+                index -= 1
+                continue
+            if flag:
+                if temp_list[index] == '1':
+                    temp_list[index] = '0'
+                else:
+                    temp_list[index] = '1'
+            index -= 1
 
-def twos_complement(b):
-    n = len(b)
-    ones = ""
-    twos = ""
-
-    # for ones complement flip every bit
-    for i in range(n):
-        ones += flip(b[i])
-
-    # for two's complement go from right
-    # to left in ones complement and if
-    # we get 1 make, we make them 0 and
-    # keep going left when we get first
-    # 0, make that 1 and go out of loop
-    ones = list(ones.strip(""))
-    twos = list(ones)
-    for i in range(n - 1, -1, -1):
-
-        if ones[i] == '1':
-            twos[i] = '0'
-        else:
-            twos[i] = '1'
-            break
-
-    i -= 1
-    # If No break : all are 1 as in 111 or 11111
-    # in such case, add extra 1 at beginning
-    if i == -1:
-        twos.insert(0, '1')
-    return ''.join(twos)
+        return ''.join(temp_list)
 
 
 instructions = {
@@ -119,10 +111,7 @@ while index < len(lines):
             binary = bin(decimal)[2:0].zfill(16)
             lines[index] = binary
         else:
-            if int(num) >= 0:
-                binary = bin(int(num))[2:].zfill(16)
-            else:
-                binary = twos_complement(bin(abs(int(num)))).zfill(16)
+            binary = decimal_to_binary(int(num), 16)
             lines[index] = binary
 
     elif 'END' in lines[index]:
@@ -151,4 +140,4 @@ for index in range(len(lines)):
 
 # printing the result
 for line in lines:
-    print(line.replace(' ',''))
+    print(line.replace(' ', ''))
