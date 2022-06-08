24b1b248c449c7a8a617f7c58cc799860c7e0b99
nova14 <saeedghanioour1380@gamil.com>
2022-06-08 12:07:57 +0430

    add a animtion for button

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const textarea = document.getElementById('input_text');
 const LineNumber = document.getElementById('counting');
 const content = document.getElementById('content');
 const dropdown = document.getElementById("drop-down")
-
+const btn = document.getElementById("btn")
+const animation = document.getElementById("center-body")
 var firstElement = ""
-
+var listDropDowon = []
 instructions = [
     // memory reference
     'AND',
@@ -44,23 +45,31 @@ textarea.addEventListener("input", (e) =>{
     var line = coutingLine(textarea)
     LineNumber.innerHTML = ""
     for(var i = 1; i <= line + 1; i++){
-            LineNumber.innerHTML += `<span >${i}</span>`
+            LineNumber.innerHTML += `<span>${i}</span>`
         }
-    console.log(e)
-    autoCompelet(e.pageX, e.pageY)
+        autoCompelet(e.pageX, e.pageY)
     
 
 })
 textarea.addEventListener('keydown', (e) => {
     if (e.key == "Tab") {
-        if (firstElement.length !=  0){
-            GetValue(firstElement)
-            firstElement = ""
-        }
+        // if (firstElement.length !=  0){
+        //     GetValue(firstElement)
+        //     firstElement = "    "
+        // }
         e.preventDefault()
-        textarea.setRangeText('  ', textarea.selectionStart, textarea.selectionEnd, "end")
+        textarea.value = textarea.value.slice(0, textarea.selectionStart) + "   " + textarea.value.slice(textarea.selectionStart, textarea.value.length);
     }  
-})
+});
+function ShowAnimation(){
+    setTimeout(() => {
+        animation.style.visibility = "visible"
+    }, 10)
+    setTimeout(() => {
+        animation.style.visibility = "hidden"
+    }, 2000)
+    
+}
 function coutingLine(textarea){
         var line = 0
         var text = textarea.value
@@ -77,11 +86,19 @@ function GetValue(value){
     var textArray = text.split(/[\s,]+/);
     var textArrayLength = textArray.length;
     var textArrayLast = textArray[textArrayLength - 1];
-    textarea.value = text.replace(textArrayLast, value);
+    textarea.value  = text.slice(0, text.length - textArrayLast.length ) + value;
     dropdown.innerHTML  = ""
     textarea.setRangeText('', textarea.selectionStart, textarea.selectionEnd, "end")
     dropdown.style.visibility = "hidden"
 
+
+}
+function SizeLineChar(textarea){
+    var text = textarea.value;
+    var textArray = text.split("\n");
+    var textArrayLength = textArray.length;
+    var textArrayLast = textArray[textArrayLength - 1];
+    return textArrayLast.length
 
 }
 
@@ -104,6 +121,7 @@ function autoCompelet(cursorX, cursorY){
     }
     
     firstElement = list[0]
+    listDropDowon = list
     if(list.length > 0) {
         dropdown.style.visibility = "visible"
         for(var i = 0; i < list.length; i++) {
@@ -111,7 +129,6 @@ function autoCompelet(cursorX, cursorY){
         }
     }else
         dropdown.style.visibility = "hidden"
-    console.log(cursorX, cursorY)
-    dropdown.style.top = cursorY  + "px"
-    dropdown.style.left = cursorX + "px"
+    dropdown.style.top = (textarea.selectionStart + 250 + coutingLine(textarea) * 22).toString()  + "px"
+    dropdown.style.left = (textarea.selectionStart + 20 + SizeLineChar(textarea) * 10.3).toString() + "px"
 }
