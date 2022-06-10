48c5f4677a70e761e61a5fa485461de6f5624da4
nova14 <saeedghanioour1380@gamil.com>
2022-06-10 22:37:31 +0430

    finished design of simulator

--- main.py
+++ main.py
@@ -10,5 +10,9 @@ a = assembler.Assembler()
 assembly_list = a.assemble(instruction_list)
 
 # printing the result
+Ram = []
 for line in assembly_list:
-    print(line.replace(' ', ''))
+    Ram.append([line[0] , line[1:4],line[5:]])
+    
+for line in Ram:
+    print(line)
